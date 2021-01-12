254bcfbb4908faab61151b2cddac90f4f4d04d70
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-01-12 12:19:11 +0530

    Add basic styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import renderView from "./render";
 import "./hamburger-menu";
 
-const dropdown = require("dropdown-odin");
-
 const views = document.querySelectorAll(".view");
 views.forEach((view) =>
   view.addEventListener("click", renderView.bind(view, view.id))
 );
-
-const more = document.querySelector("#more");
-const div = dropdown("...", ["one", "two", "three"]);
-more.appendChild(div);
