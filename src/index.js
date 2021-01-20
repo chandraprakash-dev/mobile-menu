27c732af0fe62f116db3f0bcdcdc22f72e236ca4
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-01-20 22:13:14 +0530

    Add functionality for dropdown

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ views.addEventListener("click", renderView);
 
 const homeView = document.getElementById("home");
 homeView.click();
+
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+const dropdownList = document.querySelector(".dropdown-list");
+
+function toggle() {
+  const { maxHeight } = window.getComputedStyle(dropdownList);
+  if (maxHeight === "0px") {
+    dropdownList.style.maxHeight = `${dropdownList.scrollHeight}px`;
+  } else {
+    dropdownList.style.maxHeight = null;
+  }
+}
+
+dropdownToggle.addEventListener("click", toggle);
